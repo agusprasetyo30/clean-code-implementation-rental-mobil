5b3aa76ff23e2e81df25b0ca26620746eab4e323
agusprasetyo30 <agusprasetyo1889@gmail.com>
2022-04-07 01:21:18 +0700

    menambahkan constructor pada Customer.java dan menambahkan menu pada Main.java

--- src/Tugas/Customer.java
+++ src/Tugas/Customer.java
@@ -4,6 +4,11 @@ public class Customer extends User {
     private String address;
     private String phone;
 
+    public Customer(String address, String phone) {
+        this.address = address;
+        this.phone = phone;
+    }
+
     public void setAddress(String address) {
         this.address = address;
     }
